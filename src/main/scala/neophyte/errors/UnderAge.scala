7d116cb5a272b2dc43db22de11803432814cc83c
Graeme Ludwig <graeme.ludwig@btopenworld.com>
2015-03-19 22:45:22 +0000

    Completed try examples

--- src/main/scala/neophyte/errors/UnderAge.scala
+++ src/main/scala/neophyte/errors/UnderAge.scala
@@ -5,6 +5,11 @@ class Cigarettes
 case class UnderAgeException(message:String) extends Exception(message)
 
 object Smokes extends App {
+
+  def buyCigarettes(customer: Customer): Cigarettes =
+    if (customer.age < 16) throw UnderAgeException(s"Customer must be older than 16 but was ${customer.age}")
+    else new Cigarettes
+
   def buyCigarettesWithCatch(customer: Customer) =
   try {
     buyCigarettes(customer)
@@ -13,12 +18,11 @@ object Smokes extends App {
     case UnderAgeException(msg) => msg
   }
 
-  def buyCigarettes(customer: Customer): Cigarettes =
-    if (customer.age < 16) throw UnderAgeException(s"Customer must be older than 16 but was ${customer.age}")
-    else new Cigarettes
-
+  println("[buyCigarettesWithCatch]")
   println(buyCigarettesWithCatch(Customer(21)))
   println(buyCigarettesWithCatch(Customer(14)))
+
+  println("[buyCigarettes]")
   println(buyCigarettes(Customer(21)))
   println(buyCigarettes(Customer(14)))
 }
