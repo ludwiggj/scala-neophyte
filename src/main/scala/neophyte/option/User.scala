3c8bae0a49ad5c442fbb25ad971e80b24219ec9d
Graeme Ludwig <graeme.ludwig@btopenworld.com>
2015-03-19 13:11:36 +0000

    Tidied up option examples

--- src/main/scala/neophyte/option/User.scala
+++ src/main/scala/neophyte/option/User.scala
@@ -26,9 +26,14 @@ object TestUserRepo extends App {
     if (user1.isDefined) {
       user1.get.firstName
     } else {
-      "User not there"
+      s"getUserName: User $id not there"
     }
   }
+
+  println("[getUserName]")
+  println(getUserName(1))
+  println(getUserName(2))
+  println(getUserName(3))
 
   def getUserFirstNameCareless(id: Int) = {
     val user1 = UserRepository.findById(id)
@@ -39,18 +44,27 @@ object TestUserRepo extends App {
     }
   }
 
+  println("[getUserFirstNameCareless]")
+  println(getUserFirstNameCareless(1))
+  println(getUserFirstNameCareless(2))
+  println(getUserFirstNameCareless(3))
+
   def getUserGenderOrDefault(id: Int) = {
     val user1 = UserRepository.findById(id)
 
     if (user1.isDefined) {
       user1.get.gender.getOrElse({
-        println("Evaluating default")
         "Gender not specified"
       })
     } else {
-      "User not there"
+      s"getUserGenderOrDefault: User $id not there"
     }
   }
+
+  println("[getUserGenderOrDefault]")
+  println(getUserGenderOrDefault(1))
+  println(getUserGenderOrDefault(2))
+  println(getUserGenderOrDefault(3))
 
   def getUserGenderViaMatch(id: Int) = {
     val user1 = UserRepository.findById(id)
@@ -61,9 +75,14 @@ object TestUserRepo extends App {
         case None => "Gender not specified"
       }
     } else {
-      "User not there"
+      s"getUserGenderViaMatch: User $id not there"
     }
   }
+
+  println("[getUserGenderViaMatch]")
+  println(getUserGenderViaMatch(1))
+  println(getUserGenderViaMatch(2))
+  println(getUserGenderViaMatch(3))
 
   def getUserGenderViaFor(id: Int) = {
     for {
@@ -72,11 +91,33 @@ object TestUserRepo extends App {
     } yield gender
   }
 
+  println("[getUserGenderViaFor]")
+  println(getUserGenderViaFor(1))
+  println(getUserGenderViaFor(2))
+  println(getUserGenderViaFor(3))
+
   def getUserGenderViaForDesugared(id: Int) = {
     UserRepository.findById(id).flatMap { u =>
       u.gender
     }
   }
+
+  println("[getUserGenderViaForDesugared]")
+  println(getUserGenderViaForDesugared(1))
+  println(getUserGenderViaForDesugared(2))
+  println(getUserGenderViaForDesugared(3))
+
+  def getUserGenderMapped(id: Int) = {
+    UserRepository.findById(id).map { u =>
+      u.gender
+    }
+  }
+
+  println("[getUserGenderViaForMapped]")
+
+  println(getUserGenderMapped(1))
+  println(getUserGenderMapped(2))
+  println(getUserGenderMapped(3))
 
   def getUserGenders() = {
     for {
@@ -85,94 +126,106 @@ object TestUserRepo extends App {
     } yield gender
   }
 
+  println("[getUserGenders]")
+  println(getUserGenders())
+
   def getUserGendersDesugared() = {
     UserRepository.findAll.flatMap { u =>
       u.gender
     }
   }
 
-  def getUserGendersViaMatch1() = {
+  println("[getUserGendersDesugared]")
+  println(getUserGendersDesugared())
+
+  def getUserGendersMapped() = {
+    UserRepository.findAll.map { u =>
+      u.gender
+    }
+  }
+
+  println("[getUserGendersMapped]")
+  println(getUserGendersMapped())
+
+  def getUserGendersViaMatch() = {
     for {
       User(_, _, _, _, gender) <- UserRepository.findAll
     } yield gender
   }
 
-  def getUserGendersViaMatch2() = {
+  println("[getUserGendersViaMatch]")
+  println(getUserGendersViaMatch())
+
+  def getUserGendersViaOptionMatch() = {
     for {
       User(_, _, _, _, Some(gender)) <- UserRepository.findAll
     } yield gender
   }
 
-  def getUserGendersViaMatch2Desugared() = {
+  println("[getUserGendersViaOptionMatch]")
+  println(getUserGendersViaOptionMatch())
+
+  def getUserGendersViaOptionMatchDesugared() = {
     UserRepository.findAll.flatMap { case User(_, _, _, _, gender) =>
       gender
     }
   }
 
-  def printUserName(id: Int): Unit = {
+  println("[getUserGendersViaOptionMatchDesugared]")
+  println(getUserGendersViaOptionMatchDesugared())
+
+  def printUserName(id: Int) = {
     UserRepository.findById(id).foreach(user => println(user.firstName))
   }
+
+  println("[printUserName]")
+  printUserName(1)
+  printUserName(2)
+  printUserName(3)
 
   def getUserAge(id: Int) = {
     UserRepository.findById(id).map(_.age)
   }
 
+  println("[getUserAge]")
+  println(getUserAge(1))
+  println(getUserAge(2))
+  println(getUserAge(3))
+
   def getUserGenderViaMap(id: Int) = {
     UserRepository.findById(id).map(_.gender)
   }
+
+  println("[getUserGenderViaMap]")
+  println(getUserGenderViaMap(1))
+  println(getUserGenderViaMap(2))
+  println(getUserGenderViaMap(3))
 
   def getUserGenderViaFlatMap(id: Int) = {
     UserRepository.findById(id).flatMap(_.gender)
   }
 
+  println("[getUserGenderViaFlatMap]")
+  println(getUserGenderViaFlatMap(1))
+  println(getUserGenderViaFlatMap(2))
+  println(getUserGenderViaFlatMap(3))
+
   def userOlderThat30(id: Int) = {
     UserRepository.findById(id).filter(_.age > 30)
   }
 
-  println(getUserName(1))
-  println(getUserName(2))
-  println(getUserName(3))
-  println(getUserFirstNameCareless(1))
-  println(getUserFirstNameCareless(2))
-  println(getUserFirstNameCareless(3))
-  println(getUserGenderOrDefault(1))
-  println(getUserGenderOrDefault(2))
-  println(getUserGenderOrDefault(3))
-  println(getUserGenderViaMatch(1))
-  println(getUserGenderViaMatch(2))
-  println(getUserGenderViaMatch(3))
-  printUserName(1)
-  printUserName(2)
-  printUserName(3)
-  println(getUserAge(1))
-  println(getUserAge(2))
-  println(getUserAge(3))
-  println(getUserGenderViaMap(1))
-  println(getUserGenderViaMap(2))
-  println(getUserGenderViaMap(3))
-  println(getUserGenderViaFlatMap(1))
-  println(getUserGenderViaFlatMap(2))
-  println(getUserGenderViaFlatMap(3))
+  println("[userOlderThat30]")
   println(userOlderThat30(1))
   println(userOlderThat30(2))
   println(userOlderThat30(3))
-  println(getUserGenderViaFor(1))
-  println(getUserGenderViaFor(2))
-  println(getUserGenderViaFor(3))
-  println(getUserGenderViaForDesugared(1))
-  println(getUserGenderViaForDesugared(2))
-  println(getUserGenderViaForDesugared(3))
-  println(getUserGenders())
-  println(getUserGendersDesugared())
-  println(getUserGendersViaMatch1())
-  println(getUserGendersViaMatch2())
-  println(getUserGendersViaMatch2Desugared())
 
-  case class Resource(content:String)
-  val resourceFromConfigDir:Option[Resource]= None
-  val resourceFromClasspath:Option[Resource]= Some(Resource("I was found on the classpath"))
+
+  case class Resource(content: String)
+
+  val resourceFromConfigDir: Option[Resource] = None
+  val resourceFromClasspath: Option[Resource] = Some(Resource("I was found on the classpath"))
 
   val resource = resourceFromConfigDir orElse resourceFromClasspath
-  
-  println(resource)
+
+  println(s"Resource: $resource")
 }
