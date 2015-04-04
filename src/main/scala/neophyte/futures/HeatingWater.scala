d0125837fbb2f477e1f909120a0e3d28ccfab93c
Graeme Ludwig <graeme.ludwig@btopenworld.com>
2015-04-04 21:48:09 +0100

    Futures

---
+++ src/main/scala/neophyte/futures/HeatingWater.scala
@@ -0,0 +1,83 @@
+package neophyte.futures
+
+import neophyte.futures.CoffeeFuture._
+import scala.concurrent.duration._
+import scala.concurrent.ExecutionContext.Implicits.global
+
+import scala.concurrent.{Await, Future}
+import scala.util.{Failure, Success}
+
+object HeatingWater extends App {
+
+  def temperatureOkay(heatingProcess: Water => Future[Water]): Future[Boolean] = heatingProcess(Water(25)).map {
+    water =>
+      println("we're in the future")
+      (80 to 85).contains(water.temperature)
+  }
+
+  def temperatureOkayExpectingFailure(heatingProcess: Water => Future[Water]): Future[Boolean] =
+    heatingProcess(Water(25)).failed.map {
+      failure => {
+        println(s"we're in the future and it's failed... ${failure.getMessage}")
+        false
+      }
+    }
+
+  def awaitOnWaterTemperature(heatTheWater: Future[Boolean]) = {
+    Await.ready(heatTheWater, 10.seconds).value.get match {
+      case Success(okay) => println(s"Water at correct temperature? $okay")
+      case Failure(e) => println(s"oops, no coffee today! ${e.getClass} ${e.getMessage}")
+    }
+  }
+
+  println("[temperatureOkay... heatWater]")
+  awaitOnWaterTemperature(temperatureOkay(heatWater))
+
+  println("[temperatureOkayExpectingFailure... heatWater]")
+  awaitOnWaterTemperature(temperatureOkayExpectingFailure(heatWater))
+
+  println("[temperatureOkay... heatWaterWithDodgyHeater]")
+  awaitOnWaterTemperature(temperatureOkay(heatWaterWithDodgyHeater))
+
+  println("[temperatureOkayExpectingFailure... heatWaterWithDodgyHeater]")
+  awaitOnWaterTemperature(temperatureOkayExpectingFailure(heatWaterWithDodgyHeater))
+
+  def temperatureOk(water: Water): Future[Boolean] = Future {
+    (80 to 85).contains(water.temperature)
+  }
+
+  def nestedFuture: Future[Future[Boolean]] = heatWater(Water(25)).map {
+    water => temperatureOk(water)
+  }
+
+  println("[nestedFuture...]")
+
+  Await.ready(nestedFuture, 10.seconds).value.get match {
+    case Success(containedOkay) => containedOkay.value.get match {
+      case Success(okay) => println(s"Water at correct temperature? ${okay}")
+    }
+  }
+
+  def flatFuture: Future[Boolean] = heatWater(Water(25)).flatMap {
+    water => temperatureOk(water)
+  }
+
+  println("[flatFuture...]")
+
+  Await.ready(flatFuture, 10.seconds).value.get match {
+    case Success(okay) => println(s"Water at correct temperature? ${okay}")
+  }
+
+  def coffeeComprehension: Future[Boolean] = for {
+    heatedWater <- heatWater(Water(25))
+    okay <- temperatureOk(heatedWater)
+  } yield okay
+
+  println("[coffeeComprehension...]")
+
+  Await.ready(coffeeComprehension, 10.seconds).value.get match {
+    case Success(okay) => println(s"Water at correct temperature? ${okay}")
+  }
+
+  println("Fin")
+}
---
+++ src/main/scala/neophyte/futures/package.scala
@@ -0,0 +1,14 @@
+package neophyte
+
+package object futures {
+  // Some type aliases, just for getting more meaningful method signatures:
+  type CoffeeBeans = String
+  type GroundCoffee = String
+
+  case class Water(temperature: Int)
+
+  type Milk = String
+  type FrothedMilk = String
+  type Espresso = String
+  type Cappuccino = String
+}
